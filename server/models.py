2f99ca839e5129109f704fffe33079be46fffd23
abdullahiabdi <cabudicali218@gmail.com>
2024-04-16 09:29:36 +0300

    completed

--- server/models.py
+++ server/models.py
@@ -1,8 +1,5 @@
-# server/models.py
-
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
-from sqlalchemy.ext.associationproxy import association_proxy
 
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
@@ -18,28 +15,47 @@ class Employee(db.Model):
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
+    # Relationship mapping the employee to related reviews
+    reviews = db.relationship(
+        'Review', back_populates="employee", cascade='all, delete-orphan')
+
+    # Relationship mapping employee to related onboarding
+    onboarding = db.relationship(
+        'Onboarding', uselist=False, back_populates='employee', cascade='all, delete-orphan')
+
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
 
 
-class Meeting(db.Model):
-    __tablename__ = 'meetings'
+class Onboarding(db.Model):
+    __tablename__ = 'onboardings'
 
     id = db.Column(db.Integer, primary_key=True)
-    topic = db.Column(db.String)
-    scheduled_time = db.Column(db.DateTime)
-    location = db.Column(db.String)
+    orientation = db.Column(db.DateTime)
+    forms_complete = db.Column(db.Boolean, default=False)
+
+    # Foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship mapping onboarding to related employee
+    employee = db.relationship('Employee', back_populates='onboarding')
 
     def __repr__(self):
-        return f'<Meeting {self.id}, {self.topic}, {self.scheduled_time}, {self.location}>'
+        return f'<Onboarding {self.id}, {self.orientation}, {self.forms_complete}>'
 
 
-class Project(db.Model):
-    __tablename__ = 'projects'
+class Review(db.Model):
+    __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String)
-    budget = db.Column(db.Integer)
+    year = db.Column(db.Integer)
+    summary = db.Column(db.String)
+
+    # Foreign key to store the employee id
+    employee_id = db.Column(db.Integer, db.ForeignKey('employees.id'))
+
+    # Relationship mapping the review to related employee
+    employee = db.relationship('Employee', back_populates="reviews")
 
     def __repr__(self):
-        return f'<Review {self.id}, {self.title}, {self.budget}>'
+        return f'<Review {self.id}, {self.year}, {self.summary}>'
